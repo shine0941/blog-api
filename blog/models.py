079b83db8b61b8f2556ff2a57cf9a6a3dc1407c4
shine0941 <shine55660000@gmail.com>
2025-04-18 10:44:05 +0800

    Initial commit - Django blog API setup

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
