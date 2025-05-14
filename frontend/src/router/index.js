bd64e1fda575244e6f3faad82788afa38f63dbaa
shine <shine55660000@gmail.com>
2025-05-14 17:48:20 +0800

    add create article view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Articles from '../views/Articles.vue'
+import CreateArticle from '../views/CreateArticle.vue'
 
 const routes = [
   { path: '/', component: Articles },
   { path: '/login', component: Login },
+  { path: '/createarticle', component: CreateArticle },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', // 根據你的 API 路徑調整
+  baseURL: 'http://127.0.0.1:8001/api/', // 根據你的 API 路徑調整
   withCredentials: true, // 如果你有使用 cookies 或 JWT 放在 httpOnly cookie
 })
 
